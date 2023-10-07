19d2ecdb449eb9f24adb2394b01260ef8d2b82cd
Arkadiy <goryunov.arkadiy@gmail.com>
2023-10-07 15:10:19 +0700

    Fix container imports

--- src/database/container.py
+++ src/database/container.py
@@ -1,9 +1,9 @@
 from sqlalchemy.orm import Session
 from database import engine
 
-from src.serializers.films_serializer import FilmSerializer
+from src.database.serializers.films_serializer import FilmSerializer
 
-from src.services.films_service import FilmService
+from src.database.services.films_service import FilmService
 
 
 session = Session(bind=engine)
